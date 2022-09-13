4a4950c2d6f4ce017f92c81c4d47c643fbea1105
Bagas Miftahun Na'im <bagasmiftahunaim@gmail.com>
2022-09-13 14:35:45 +0700

    Update Database migration and Models

--- database/migrations/2022_09_13_072740_create_attendance_table.php
+++ database/migrations/2022_09_13_072740_create_attendance_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('attendance', function (Blueprint $table) {
             $table->id();
+            $table->user_id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('description');
+            $table->intger('employes_id');
+            $table->string('image');
+            $table->string('code');
+            $table->timestamps('time_scan');
         });
     }
 
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('attendance');
     }
 };
