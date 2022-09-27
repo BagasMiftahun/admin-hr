42816da2479d5e7eab066b21e83c617e1b32a691
aananipah_ <aananipah>
2022-09-27 14:27:15 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\AbsensiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 // Confirm Email
 Route::get('/confirm-email/{token}', [AuthController::class, 'confirm'])->name('confirm');
+
+// Absensi
+Route::get('/absensi', [AbsensiController::class, 'index']);
+Route::get('absensi-create', [AbsensiController::class, 'create']);
+Route::get('absensi-destroy/{id}', [AbsensiController::class, 'destroy']);
+Route::get('absensi-edit.{id}', [AbsensiController::class, 'edit']);
+Route::post('absensi-store', [AbsensiController::class, 'store']);
