27e53b43c6e63e36ee075a2390c5039adf19288f
Tifanti <tifantinurr@gmail.com>
2022-09-28 13:41:22 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::resource('/pekerjaans', \App\Http\Controllers\PekerjaanController::class)->middleware('auth');
+Route::get('/', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'login'])->name('login.proses')->middleware('guest');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
 
 //route resource
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
